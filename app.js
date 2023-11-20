d50576c865068bbf44071fc0d47c4ab3e3b522a4
vmeet24 <meetvora1998@gmail.com>
2023-11-20 00:24:32 -0500

    a5 commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*"}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
