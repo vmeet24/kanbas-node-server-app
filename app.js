08b44041004ef1f1a83f762c2df0a2399871858a
vmeet24 <meetvora1998@gmail.com>
2023-12-04 20:39:42 -0500

    a6 commit

--- app.js
+++ app.js
@@ -5,11 +5,42 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+
+const CONNECTION_STRING = `mongodb+srv://meetvora1998:family123@cluster0.shbqjgq.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
 const app = express();
-app.use(cors({ origin: "*"}));
+app.use(session(sessionOptions));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
